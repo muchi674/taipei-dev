d0416d4f997925b8a389c796e4698b8319aea3de
muchichen <muchi674@gmail.com>
2023-05-13 14:48:20 +0800

    add navHeader & import GIS lib

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import NavHeader from "./components/NavHeader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavHeader />
+      <main>
+        <Routes>
+          <Route path="/home" element={<h1>I'm Rick James, bitch</h1>} />
+          <Route path="users/:userId/lots" element={<h1>Tyrone Biggums</h1>} />
+          <Route
+            path="users/:userId/bids"
+            element={<h1>Cocaine is hell of a drug</h1>}
+          />
+          <Route path="/auth" element={<h1>log in with Google</h1>} />
+          <Route path="/*" element={<Navigate replace to="/home" />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/NavHeader.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavHeader() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/home">
+          bakiAuctions
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="users/0/lots">
+              my lots
+            </Nav.Link>
+            <Nav.Link as={Link} to="users/0/bids">
+              my bids
+            </Nav.Link>
+            <Nav.Link as={Link} to="/auth">
+              log in
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavHeader;
